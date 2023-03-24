a12239ce9b5ed33e518fd3f9381878b76a2a3445
Tobias Buschor <tobias.buschor@shwups.ch>
2023-03-24 01:23:58 +0100

    Update mod.js

--- mod.js
+++ mod.js
@@ -48,100 +48,104 @@ export function dump(obj, {depth=6, enumerable=false, inherited=true, order=fals
             case 'number': return '<number>'+encode(obj)+'<number>';
             case 'boolean': return '<bool>'+encode(obj)+'<bool>';
             case 'symbol': return '<symbol>'+encode(obj.toString())+'<symbol>';
-            //case 'function': return '<function>function '+encode(obj.name)+'<function>';
-            default:
-
-                if (typeof obj === 'function' && !isConstructor(obj)) return '<function>function '+encode(obj.name)+'<function>';
-                if (obj == null) return '<null>'+obj+'<null>';
-                if (obj instanceof Date) return '<date>'+obj+'<date>';
-
-                if (objects.has(obj)) return '<a href="#'+objects.get(obj)+'">(circular)</a>';
-
-                // its an object / array
-                if (level > depth) return '...';
-
-                const id = ('x'+Math.random()).replace('.','');
-                try { objects.set(obj, id); }
-                catch { return '? error ?'; }
-
-                let keys = {};
-                const ownKeys = enumerable ? Object.keys(obj) : Object.getOwnPropertyNames(obj);
-                for (let k of ownKeys) keys[k] = 'own';
-                if (inherited) {
-                    for (let k in obj) if (!keys[k]) keys[k] = 'inherited';
-                }
-                if (order) {
-                    let nKeys = {};
-                    const tmp = Object.keys(keys).sort();
-                    for (let k of tmp) nKeys[k] = keys[k];
-                    keys = nKeys;
-                }
-
-                let isArray = Array.isArray(obj) && obj !== Array.prototype;
-
-
-                // table
-                var cols = objectIsTable(obj);
-                if (cols) {
-                    let str = '<table id="'+id+'">';
-                    str += '<thead>';
-                    str += '<tr>';
-                    str += '<td><small>' + (isArray?'(items)':'(index)')+'</small>';
-                    for (let col in cols) {
-                        str += '<td>'+ encode(col);
-                    }
-                    str += '<tbody>';
-                    for (let name in keys) {
-                        if (isArray && name==='length') continue;
-                        let value = obj[name];
-                        str += '<tr>';
-                        str += '<td>';
-                        str += encode(name) + (isArray ? ' = { ' : ' : { ');
-                        for (let col in cols) {
-                            str += '<td>'
-                            str += (col in value) ? valueToHtml(value[col], level) : '<null>(not set)</null>';
-                        }
-                    }
-                    return str += '</table>';
-                }
-
-                if (isArray) {
-                    return '[' + obj.map(item => valueToHtml(item, level)).join(' , ') + ']';
-                } else { // object
-                    // object
-                    let str = '<table id="'+id+'">';
-                    for (let name in keys) {
-                        let value = null;
-                        try { value = obj[name]; }
-                        catch (e) { value = '? error ?' }
-                        str += '<tr>';
-                        str += '<td>'+encode(name);
-                        if (keys[name] == 'inherited') str += ' <small>(inherited)</small>';
-                        str += '<td>'+valueToHtml(value, level);
-                    }
-                    return str += '</table>';
-                }
         }
+
+        if (typeof obj === 'function' && !isConstructor(obj)) {
+            const asStr = obj.toString();
+            const arrow =  asStr[0]==='f' || asStr.replace(/\s+/g,' ').startsWith('async function') ? '' : 'arrow';
+            const async = obj[Symbol.toStringTag] === 'AsyncFunction' ? 'async ' : '';
+            const generator = obj[Symbol.toStringTag] === 'GeneratorFunction' ? '*' : '';
+            return '<function>'+async+' '+arrow+' '+generator+'function <b>'+encode(obj.name)+'</b>('+obj.length+')<function>';
+        }
+        if (obj == null) return '<null>'+obj+'<null>';
+        if (obj instanceof Date) return '<date>'+obj+'<date>';
+
+        if (objects.has(obj)) return '<a href="#'+objects.get(obj)+'">(circular)</a>';
+
+        // its an object / array
+        if (level > depth) return '...';
+
+        const id = ('x'+Math.random()).replace('.','');
+        try { objects.set(obj, id); }
+        catch { return '? error ?'; }
+
+        let keys = {};
+        const ownKeys = enumerable ? Object.keys(obj) : Object.getOwnPropertyNames(obj);
+        for (const k of ownKeys) keys[k] = 'own';
+        if (inherited) {
+            for (const k in obj) if (!keys[k]) keys[k] = 'inherited';
+        }
+        if (order) {
+            const nKeys = {};
+            const tmp = Object.keys(keys).sort();
+            for (const k of tmp) nKeys[k] = keys[k];
+            keys = nKeys;
+        }
+
+        const isArray = Array.isArray(obj) && obj !== Array.prototype;
+
+        // table
+        const cols = objectIsTable(obj);
+        if (cols) {
+            let str = '<table id="'+id+'">';
+            str += '<thead>';
+            str += '<tr>';
+            str += '<td><small>' + (isArray?'(items)':'(index)')+'</small>';
+            for (const col in cols) {
+                str += '<td>'+ encode(col);
+            }
+            str += '<tbody>';
+            for (const name in keys) {
+                //if (isArray && name==='length') continue;
+                const value = obj[name];
+                if (name==='length' && typeof value === 'number') continue;
+                str += '<tr>';
+                str += '<td>';
+                str += encode(name) + (isArray ? ' = { ' : ' : { ');
+                for (const col in cols) {
+                    str += '<td>'
+                    str += (col in value) ? valueToHtml(value[col], level) : '<null>(not set)</null>';
+                }
+            }
+            return str += '</table>';
+        }
+
+        if (isArray) {
+            return '[' + obj.map(item => valueToHtml(item, level)).join(' , ') + ']';
+        } else { // object
+            let str = '<table id="'+id+'">';
+            for (const name in keys) {
+                let value = null;
+                try { value = obj[name]; }
+                catch { value = '? error ?' }
+                str += '<tr>';
+                str += '<td>'+encode(name);
+                if (keys[name] == 'inherited') str += ' <small>(inherited)</small>';
+                str += '<td>'+valueToHtml(value, level);
+            }
+            return str += '</table>';
+        }
+
     }
 
     function objectIsTable(obj) {
         const keys = {}; // cols
-        let numProps = 0;;
-        for (let prop in obj) {
+        let numProps = 0;
+        for (const prop in obj) {
             try {
                 if (typeof obj[prop] === 'string') return;
                 ++numProps;
-                for (let key in obj[prop]) {
+                for (const key in obj[prop]) {
                     if (keys[key] === undefined) keys[key] = 0;
                     keys[key]++;
                 }
-            } catch (e) {
+            } catch {
                 //console.log(obj, e)
             }
         }
         if (numProps < 3) return; // just two rows
         if (Object.values(keys).length < 2) return; // not enough cols
-        for (let keyNum of Object.values(keys)) {
+        for (const keyNum of Object.values(keys)) {
             if (keyNum < numProps/2) { // not minimum half the keys in sub obj
                 return;
             }
@@ -150,25 +154,21 @@ export function dump(obj, {depth=6, enumerable=false, inherited=true, order=fals
     }
 }
 
-export function encode(str){ // ttodo: does not escape " and '
+export function encode(str){ // TODO: does not escape " and '
     return (str+'').replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
         return '&#'+i.charCodeAt(0)+';';
     });
 }
 
-/* */
+
 function isConstructor(f) {
     if (typeof f !== 'function') return false;
-    try {
-        Reflect.construct(String, [], f);
-    } catch (e) {
-        return false;
-    }
+    if (f.prototype === undefined) return false;
+    if (Reflect.ownKeys(f.prototype).length < 2) return false;
+    // try {
+    //     Reflect.construct(String, [], f);
+    // } catch {
+    //     return false;
+    // }
     return true;
 }
-/**
-function isConstructor(obj) {
-    if (typeof f !== 'function') return false;
-    return !!obj.prototype && !!obj.prototype.constructor.name;
-}
-/* */
